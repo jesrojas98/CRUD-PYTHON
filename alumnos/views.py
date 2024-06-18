3c7ce7423198025dc07268c9c3558ab1a166ae29
jesrojas98 <jes.rojasg@duocuc.cl>
2024-06-17 21:03:30 -0400

    17-06 funcionando todo el crud

--- alumnos/views.py
+++ alumnos/views.py
@@ -13,38 +13,40 @@ def index(request):
     context={"alumnos":alumnos}
     return render(request, 'alumnos/index.html', context)
 
-
 def alumnosAdd(request):
-    if request.method is not "POST":
-        alumnos=Alumno.objects.all()
-        context={"alumnos":alumnos}
-        return render(request, 'alumnos/alumnos_add.html', context)
+    print('Ingresando a createalumno...')
+    print(request.method)
+    if request.method == 'POST':
+        print('Ingresando a POST...')
+        print(request)
+        print(request.POST['rut'])
+        print(request.POST['nombrealumno'])
+        _rut = request.POST['rut']
+        _nombre = request.POST['nombrealumno']
+        _aPaterno = request.POST['paterno']
+        _aMaterno = request.POST['materno']
+        _fechaNac = request.POST['fechaNac']
+        _telefono = request.POST['telefono']
+        _email = request.POST['email']
+        _direccion = request.POST['direccion']
+        _activo = 1
+        alumno = Alumno.objects.create(
+            rut=_rut,
+            nombre=_nombre,
+            apellido_paterno=_aPaterno,
+            apellido_materno=_aMaterno,
+            fecha_nacimiento=_fechaNac,
+            telefono=_telefono,
+            email=_email,
+            direccion=_direccion,
+            activo=_activo
+        )
+        alumno.save()
+        return render(request, 'alumnos/alumnos_add.html')
     else:
-        rut=request.POST["rut"]
-        nombre=request.POST["nombre"]
-        aPaterno=request.POST["paterno"]
-        aMaterno=request.POST["materno"]
-        fechaNac=request.POST["fechaNac"]
-        telefono=request.POST["telefono"]
-        email=request.POST["email"]
-        direccion=request.POST["direccion"]
-        activo="1"
-
-        obj=Alumno.objects.create(rut=rut,
-                                  nombre=nombre,
-                                  apellido_paterno=aPaterno,
-                                  apellido_materno=aMaterno,
-                                  fecha_nacimiento=fechaNac,
-                                  telefono=telefono,
-                                  email=email,
-                                  direccion=direccion,
-                                  activo=1)
-        
-        obj.save()
-        context={'mensaje':"ok, Datos Guardados..."}
-        return render(request, 'alumnos/alumnos_add.html', context)
+        print('Ingresando a GET...')
+        return render(request, 'alumnos/alumnos_add.html')
     
-
 def alumnos_del(request,pk):
     context={}
     try:
@@ -62,45 +64,46 @@ def alumnos_del(request,pk):
         return render(request, 'alumnos/alumnos_list.html', context)
     
 def alumnos_findEdit(request,pk):
+    
     if pk != "":
         alumno=Alumno.objects.get(rut=pk)
-       
         context={'alumno':alumno}
         if alumno:
             return render(request, 'alumnos/alumnos_edit.html', context)
-        else:
-            context={'mensaje':'Error: Rut no existe...'}
-            return render(request, 'alumnos/alumnos_edit.html', context)
-        
-def alumnosUpdate(request):
-        if request.method=="POST":
-            rut=request.POST["rut"]
-            nombre=request.POST["nombre"]
-            aPaterno=request.POST["paterno"]
-            aMaterno=request.POST["materno"]
-            fechaNac=request.POST["fechaNac"]
-            telefono=request.POST["telefono"]
-            email=request.POST["email"]
-            direccion=request.POST["direccion"]
-            activo="1"
+
+        else:  
+            context={'mensaje: '"Error rut no existe..."}
+            return render(request, 'alumnos/alumnos_list.html', context)
+
+
+
+def alumnosUpdate (request):
+    if request.method == "POST":
+            _rut = request.POST["rut"]
+            _nombre = request.POST["nombrealumno"]
+            _aPaterno = request.POST['paterno']
+            _aMaterno = request.POST['materno']
+            _fechaNac = request.POST['fechaNac']
+            _telefono = request.POST['telefono']
+            _email = request.POST['email']
+            _direccion = request.POST['direccion']
+            _activo = 1
 
             alumno=Alumno()
-            alumno.rut=rut
-            alumno.nombre=nombre
-            alumno.apellido_paterno=aPaterno
-            alumno.apellido_materno=aMaterno
-            alumno.fecha_nacimiento=fechaNac
-            alumno.telefono=telefono
-            alumno.email=email
-            alumno.direccion=direccion
+            alumno.nombre=_nombre
+            alumno.rut=_rut
+            alumno.apellido_paterno=_aPaterno
+            alumno.apellido_materno=_aMaterno
+            alumno.fecha_nacimiento=_fechaNac
+            alumno.telefono=_telefono
+            alumno.email=_email
+            alumno.direccion=_direccion
             alumno.activo=1
             alumno.save()
             context={"mensaje": "Ok, datos actualizados", 'alumno':alumno}
             return render(request, 'alumnos/alumnos_edit.html', context)
-        else:
-            alumnos=Alumno.objects.all()
-            context={'alumnos':alumnos}
-            return render(request, 'alumnos/alumnos_list.html', context)
-
-       
-
+    else:
+        alumnos=Alumno.objects.all()
+        context={'mensaje':'Error: Rut no existe...'}
+        return render(request, 'alumnos/alumnos_edit.html', context)
+        
